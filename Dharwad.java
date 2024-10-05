a586a29ad0f4100d87feec23cf5c54e8cb4d99d5
CORP\Ravi.Tripathi <ravi.tripathi@Powerschool.com>
2024-10-05 22:19:33 +0530

    Soubhagya file---

--- Dharwad.java
+++ Dharwad.java
@@ -4,6 +4,6 @@ public class Dharwad
 {
 	public static void main(String args[])
 	{
-		System.out.println("My home name is  : "+args[1]);
+		System.out.println(" My home name is  : "+args[1]);
 	}
 }
